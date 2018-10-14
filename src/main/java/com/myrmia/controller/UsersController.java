fef293e62f9ea2c60471bbe6ea9999a7dc979623
Ellery Zhang <allotory@msn.com>
2018-10-14 22:25:10 +0800

    feat: add freemarker support

--- src/main/java/com/myrmia/controller/UsersController.java
+++ src/main/java/com/myrmia/controller/UsersController.java
@@ -1,8 +1,12 @@
 package com.myrmia.controller;
 
+import com.myrmia.model.UsersDO;
 import com.myrmia.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * users controller
@@ -14,6 +18,18 @@ public class UsersController {
     private UsersService usersService;
 
 
+    @RequestMapping(value="/test", method = RequestMethod.GET)
+    public String test(Model model) {
+//        UsersDO usersDO = new UsersDO();
+//        usersDO.setUsername("test");
+//        usersDO.setPassword("test");
+//        usersDO.setCreated(1);
+//        usersService.saveUsers(usersDO);
+
+        model.addAttribute("name", "ellery");
+
+        return "test";
+    }
 
     @Autowired
     public void setUsersService(UsersService usersService) {
---
+++ src/main/webapp/WEB-INF/templates/test.ftl
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World! Hello ${name}!</h2>
+
+</body>
+</html>

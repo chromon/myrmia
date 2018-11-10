ca49a950761e8eefbc50719ceb0c79c764456ee5
Ellery Zhang <allotory@msn.com>
2018-11-10 22:28:36 +0800

    feat: add db sql file

--- src/main/resources/db/myrmia.sql
+++ src/main/resources/db/myrmia.sql
@@ -18,3 +18,87 @@ CREATE TABLE t_users (
     logged INTEGER (10),
     group_name VARCHAR (16)
 );
+
+DROP TABLE IF EXISTS t_contents;
+CREATE TABLE t_contents (
+    cid INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+    title VARCHAR (255) NOT NULL,
+    slug VARCHAR (255) UNIQUE,
+    thumb_img VARCHAR(255),
+    created INTEGER (10) NOT NULL,
+    modified INTEGER (10),
+    content TEXT,
+    author_id INTEGER (10) NOT NULL,
+    type VARCHAR (16) NOT NULL,
+    status VARCHAR (16) NOT NULL,
+    fmt_type VARCHAR (16) DEFAULT ('markdown'),
+    tags VARCHAR (200),
+    categories VARCHAR (200),
+    hits INTEGER (10) DEFAULT (0),
+    comments_num INTEGER (1) DEFAULT (0),
+    allow_comment INTEGER (1) DEFAULT (1),
+    allow_ping INTEGER (1),
+    allow_feed INTEGER (1)
+);
+
+DROP TABLE IF EXISTS t_comments;
+CREATE TABLE t_comments (
+    coid INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    cid INTEGER DEFAULT (0) NOT NULL,
+    created INTEGER (10) NOT NULL,
+    author VARCHAR (200) NOT NULL,
+    author_id INTEGER (10) DEFAULT (0),
+    owner_id INTEGER (10) DEFAULT (0),
+    mail VARCHAR (200) NOT NULL,
+    url VARCHAR (200),
+    ip VARCHAR (64),
+    agent VARCHAR (200),
+    content TEXT NOT NULL,
+    type VARCHAR (16),
+    status VARCHAR (16),
+    parent INTEGER (10) DEFAULT (0)
+);
+
+DROP TABLE IF EXISTS t_logs;
+CREATE TABLE t_logs (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL,
+    action VARCHAR (100) NOT NULL,
+    data VARCHAR (2000),
+    author_id INTEGER (10) NOT NULL,
+    ip VARCHAR (20),
+    created INTEGER (10) NOT NULL
+);
+
+DROP TABLE IF EXISTS t_metas;
+CREATE TABLE t_metas (
+    mid INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL,
+    name VARCHAR (200) NOT NULL,
+    slug VARCHAR (200),
+    type VARCHAR (32) NOT NULL,
+    description VARCHAR (255),
+    sort INTEGER (4) DEFAULT (0),
+    parent INTEGER (10) DEFAULT (0)
+);
+
+DROP TABLE IF EXISTS t_options;
+CREATE TABLE t_options (
+    name VARCHAR (100) PRIMARY KEY UNIQUE NOT NULL,
+    value TEXT,
+    description VARCHAR (255)
+);
+
+DROP TABLE IF EXISTS t_relationships;
+CREATE TABLE t_relationships (
+    cid INTEGER (10) NOT NULL,
+    mid INTEGER (10) NOT NULL
+);
+
+DROP TABLE IF EXISTS t_attach;
+CREATE TABLE t_attach (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    fname VARCHAR (100) NOT NULL,
+    ftype VARCHAR (50),
+    fkey VARCHAR (100) NOT NULL,
+    author_id INTEGER (10) NOT NULL,
+    created INTEGER (10) NOT NULL
+);
